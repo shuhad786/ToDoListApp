08aec59cd7cb243b95937dbecaf029f3cf34d975
Shuhad786 <shuhad.loofer@gmail.com>
2022-10-13 11:11:48 +0200

    completed edit function testing

--- app.test.js
+++ app.test.js
@@ -1,4 +1,4 @@
-import { addList, displayList } from './src/modules/app.js';
+import { addList, displayList, updateList } from './src/modules/app.js';
 
 describe('Add and Remove Testing', () => {
   document.body.innerHTML = `
@@ -22,5 +22,25 @@ describe('Add and Remove Testing', () => {
     displayList();
     const todolist = document.getElementById('toDoListItemContainer');
     expect(todolist.childElementCount).toBe(1);
+  });
+});
+
+  describe('Add and Remove Testing', () => {
+    document.body.innerHTML = `
+    <input id='toDoInput'>
+    <button id='addBtn'></button>
+    <div id='toDoListItemContainer'></div>
+    `;
+
+  test('Should update description', () => {
+    const localGet = JSON.parse(localStorage.getItem('listStorage'));
+    addList('paint', false, 0);
+    expect(localGet.length).toBe(1);
+    localGet[0].description = 'changed';
+    localStorage.setItem('listStorage', JSON.stringify(localGet));
+    displayList();
+    const todolist = document.getElementById('toDoListItemContainer');
+    console.log(localGet)
+    expect(todolist.children[0].children[1].value).toBe('changed');
   });
 });
